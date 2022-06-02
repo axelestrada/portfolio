7e1b0e30f39e3aedee60ce6f0745f12499fd5fd7
Axel Estrada <axele1524@gmail.com>
2022-06-02 16:00:19 -0600

    Cambio de las rutas en /pages/blog.tsx y eliminacion de package-lock.json

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,10 +1,10 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 
-import Navbar from "/src/components/Navbar";
+import Navbar from "../src/components/Navbar";
 
-import Profile from "/src/sections/Profile";
-import BlogSection from "/src/sections/Blog";
+import Profile from "../src/sections/Profile";
+import BlogSection from "../src/sections/Blog";
 
 const Blog: NextPage = () => {
   return (
