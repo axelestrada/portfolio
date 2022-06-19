7ca2b9ed4774fca348122f86f407bbccbe2748cd
Axel Estrada <axele1524@gmail.com>
2022-06-19 12:36:31 -0600

    Uso de Formik en el formulario de la Newssletter

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -1,3 +1,7 @@
+// Formik
+import { useFormik } from "formik";
+import * as yup from "yup";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 
@@ -5,6 +9,21 @@ import Button from "./Button";
 import { Input } from "./FormFields";
 
 export default function Newsletter() {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+    },
+    onSubmit: () => {
+      console.log(formik.values.email)
+    },
+    validationSchema: yup.object({
+      email: yup
+        .string()
+        .email("Ingrese una direccion de email valida")
+        .required("Por favor ingrese su email"),
+    }),
+  });
+
   return (
     <div className="newsletter">
       <p className="text">
@@ -12,10 +31,19 @@ export default function Newsletter() {
         suscribase al boletin informativo
       </p>
 
-      <form className="form">
-        <Input icon={faEnvelope} type="email" placeholder="Email" />
+      <form className="form" onSubmit={formik.handleSubmit}>
+        <Input
+          type="email"
+          name="email"
+          icon={faEnvelope}
+          placeholder="Email"
+          error={formik.errors.email}
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
 
-        <Button title="Suscribirse" />
+        <Button title="Suscribirse" buttonType="submit" />
       </form>
     </div>
   );
